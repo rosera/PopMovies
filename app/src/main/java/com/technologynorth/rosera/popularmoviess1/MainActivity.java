8daa1d24b26154494aae68fdd0df2497087687d5
rosera <technologynorth@gmail.com>
2016-05-02 23:54:21 +0100

    Added fragment XML and boilerplate code for calling the fragment.

--- app/src/main/java/com/technologynorth/rosera/popularmoviess1/MainActivity.java
+++ app/src/main/java/com/technologynorth/rosera/popularmoviess1/MainActivity.java
@@ -1,13 +1,26 @@
 package com.technologynorth.rosera.popularmoviess1;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements MovieFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Todo: Add a call to fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.movies_list, new MovieFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        return;
     }
 }
