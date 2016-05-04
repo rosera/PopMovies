b399b11ea6fc81d0b6cdb8229d25a27bb7208f82
rosera <technologynorth@gmail.com>
2016-05-04 08:58:32 +0100

    Fixed error in XML definition of main_activity.xml

--- app/src/main/java/com/technologynorth/rosera/popularmoviess1/MovieAdapter.java
+++ app/src/main/java/com/technologynorth/rosera/popularmoviess1/MovieAdapter.java
@@ -1,10 +1,14 @@
 package com.technologynorth.rosera.popularmoviess1;
 
 import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -47,14 +51,49 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
 
     @Override
     public View getView(int position, View view, ViewGroup parent) {
+        ViewHolder  filmViewHolder;
+        View gridView;
 
-        ImageView imageView = new ImageView(mActivity);
+        LayoutInflater inflater = (LayoutInflater)
+                mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (view == null) {
+            gridView = new View(mActivity);
+
+            gridView = inflater.inflate(R.layout.gridview_image, null);
+            gridView.setLayoutParams(new GridView.LayoutParams(350,475));
+            ImageView imageView = (ImageView)gridView.findViewById(R.id.imageViewMovie);
+            TextView textView = (TextView)gridView.findViewById(R.id.textViewMovie);
+
+            imageView.setImageResource(R.drawable.antman);
+            textView.setText("Antman");
+        }
+        else {
+            gridView = (View)view;
+        }
 
         // TODO: Implement the ViewHolder pattern - see GridView bookmark StackOverflow
+//        if (view == null) {
+//            view = LayoutInflater.from(mActivity).inflate(R.layout.gridview_image, null);
+//            view.setLayoutParams(new GridView.LayoutParams(350,475));
+//
+//            filmViewHolder = new ViewHolder();
+//
+//            filmViewHolder.mImageViewFilm   = (ImageView) view.findViewById(R.id.imageViewMovie);
+//            view.setTag(filmViewHolder);
+//        } else {
+//            filmViewHolder = (ViewHolder) view.getTag();
+//        }
 
-        // Set the information
-        imageView.setImageResource(R.drawable.antman);
 
-        return imageView;
+//        if (mMovies.get(position) != null) {
+//            filmViewHolder.mImageViewFilm.setImageResource(R.drawable.antman);
+////            ImageView imageView = new ImageView(mActivity);
+//
+//            // Set the information
+//  //          imageView.setImageResource(R.drawable.antman);
+//        }
+
+        return gridView;
     }
 }
